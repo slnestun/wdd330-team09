a5a9cd56f78379701c469670e5bd777a976ad42f
essunombre <alb21001@byui.edu>
2024-04-27 21:29:24 -0500

    Fixing Product array for cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,20 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+
+  //get the existing cart items from local storage
+  let existingCart = getLocalStorage('so-cart');
+  //If the existing cart is not an array (because is empty)
+  //Initialize it as an array empty
+  if (!Array.isArray(existingCart)){
+    existingCart = [];
+  }
+  // Add the product
+  existingCart.push(product)
+  setLocalStorage('so-cart', existingCart)
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
