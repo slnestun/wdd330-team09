9294aef04b8f88ff6410ece9a8bcb6b6f9b1c67c
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-02 12:19:45 -0300

    Doing again week 1

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,19 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getLocalStorage, setLocalStorage, getParams } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-
   //get the existing cart items from local storage
-  let existingCart = getLocalStorage('so-cart');
+  let existingCart = getLocalStorage("so-cart");
   //If the existing cart is not an array (because is empty)
   //Initialize it as an array empty
-  if (!Array.isArray(existingCart)){
+  if (!Array.isArray(existingCart)) {
     existingCart = [];
   }
   // Add the product
-  existingCart.push(product)
-  setLocalStorage('so-cart', existingCart)
+  existingCart.push(product);
+  setLocalStorage("so-cart", existingCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -24,5 +23,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
