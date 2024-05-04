6fa04c70a28fe6174078c5a31b8461b8acd5ec0d
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-04 10:32:45 -0300

    copy the finish w2team

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,39 @@
-import { getLocalStorage, setLocalStorage, getParams } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs'
+import { getParams } from './utils.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
+const productId = getParams('product');
 
-function addProductToCart(product) {
-  //get the existing cart items from local storage
-  let existingCart = getLocalStorage("so-cart");
-  //If the existing cart is not an array (because is empty)
-  //Initialize it as an array empty
-  if (!Array.isArray(existingCart)) {
-    existingCart = [];
-  }
-  // Add the product
-  existingCart.push(product);
-  setLocalStorage("so-cart", existingCart);
-}
+const product = new ProductDetails(productId, dataSource)
+product.init();
+
+// Testing getParams and variables
+// console.log(product.productId);
+// console.log(product.product);
+// console.log(typeof product.product);
+
+//Old addProductToCart now addToCart
+// addProductToCart(product){
+//   console.log('you clicked me')
+//   //get the existing cart items from local storage
+//   let existingCart = getLocalStorage('so-cart');
+//   //If the existing cart is not an array (because is empty)
+//   //Initialize it as an array empty
+//   if (!Array.isArray(existingCart)){
+//     existingCart = [];
+//   }
+//   // Add the product
+//   existingCart.push(product)
+//   setLocalStorage('so-cart', existingCart)
+// };
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// document.getElementById('addToCart').addEventListener('click', this.addToCart);
+  
