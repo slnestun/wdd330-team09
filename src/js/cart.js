44da1fa31f91cb3bdde6f8e7e03bc59fadaa2de9
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-10 01:39:58 -0300

    team actuvity practice

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,30 +1,10 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
-  // we getItem to show the number not the list of card and show on the bag
-  document.querySelector(".icon-cart").innerHTML = localStorage.getItem('so-cart-quantity');
-}
+loadHeaderFooter();
 
-function cartItemTemplate(item) {
-  const newItem = `<li class='cart-card divider'>
-  <a href='#' class='cart-card__image'>
-    <img
-      src='${item.Image}'
-      alt='${item.Name}'
-    />
-  </a>
-  <a href='#'>
-    <h2 class='card__name'>${item.Name}</h2>
-  </a>
-  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty: ${item.quantity}</p>
-  <p class='cart-card__price'>$${item.FinalPrice}</p>
-</li>`;
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.renderCartContents();
 
-  return newItem;
-}
-
-renderCartContents();
+// we getItem to show the number not the list of card and show on the bag
+document.querySelector(".icon-cart").innerHTML = localStorage.getItem('so-cart-quantity') || 0;
