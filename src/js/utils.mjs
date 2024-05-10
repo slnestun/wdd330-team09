5a0fcb6055cc7a9039b7d977fb330e3f9a05be3a
Tomas Elias Fontes <90219439+TomasEliasFontes@users.noreply.github.com>
2024-05-10 01:16:35 -0300

    W03 Team Activity: Dynamic Header and Footer (#13)

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -43,3 +43,29 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
+
+export function renderWithTemplate(template,parentElement,data,callback) 
+{
+  parentElement.insertAdjacentHTML("afterbegin", template)
+  // if clear is true we need to clear out the contents of the parent.
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// function to dynamically load the header and footer into a page
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
