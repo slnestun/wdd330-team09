e543af7bd746f4bcea29c16c678cd765a5e553ff
essunombre <alb21001@byui.edu>
2024-05-13 18:10:23 -0500

    fixing quotes

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
