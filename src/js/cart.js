b221ef5f6d3f4ba8a9d165607c6d0450ef79e24c
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-14 12:08:36 -0300

    add single quotes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,10 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
-import ShoppingCart from "./ShoppingCart.mjs";
+import { getLocalStorage, loadHeaderFooter } from './utils.mjs';
+import ShoppingCart from './ShoppingCart.mjs';
 
 loadHeaderFooter();
 
-const cart = new ShoppingCart("so-cart", ".product-list");
+const cart = new ShoppingCart('so-cart', '.product-list');
 cart.renderCartContents();
 
 // we getItem to show the number not the list of card and show on the bag
-document.querySelector(".icon-cart").innerHTML = localStorage.getItem('so-cart-quantity') || 0;
+document.querySelector('.icon-cart').innerHTML = localStorage.getItem('so-cart-quantity') || 0;
