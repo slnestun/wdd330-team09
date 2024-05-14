b8bd784e29dbf8e11137e990e2d87245b83f7d5a
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-14 12:10:48 -0300

    Create product listing to show all product in each category

---
+++ src/js/product-listing.js
@@ -0,0 +1,23 @@
+import {loadHeaderFooter, getParams } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+loadHeaderFooter();
+
+const category = getParams('category');
+const dataSource = new ProductData();
+const element = document.querySelector('.product-list');
+const listing = new ProductList(category, dataSource, element)
+
+document.addEventListener('DOMContentLoaded', function() {
+    let titleElement = document.querySelector('.products > h2');
+    let urlCategory = new URL(window.location.href);
+    let searchCategory = new URLSearchParams(urlCategory.search)
+    let categoryTitle = searchCategory.get('category')
+    titleElement.textContent = `Top Products: ${categoryTitle}`
+  });
+
+listing.init();
+
+// we getItem to show the number not the list of card and show on the bag
+document.querySelector('.icon-cart').innerHTML = localStorage.getItem('so-cart-quantity') || 0;
