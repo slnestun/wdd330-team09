76a575e119e9ba32beb45682e3c5dd150464536c
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-05-14 12:13:02 -0300

    images sizes

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -4,7 +4,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Image.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -27,6 +27,6 @@ export default class ShoppingCart {
   renderCartContents() {
     const cartItems = getLocalStorage(this.key);
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+    document.querySelector(this.parentSelector).innerHTML = htmlItems.join('');
   }
 }
