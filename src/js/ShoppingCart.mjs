f18f07532e2d44b43cc52dae26e8513e1c014ca7
Jose Albancando Robles <78940115+essunombre@users.noreply.github.com>
2024-05-16 20:24:47 -0500

    addign single quotes an image on cart (#19)
    Co-authored-by: slnestun <97054774+slnestun@users.noreply.github.com>

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -7,7 +7,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${productImage}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
