66da4cf18df3d32c4b3434189d2a42049b1bdaef
slnestun <riv21007@byui.edu>
2024-05-18 16:18:49 -0300

    fix update quantity on cart icon and add animation when a product is added from list or from + button on cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -12,4 +12,4 @@ handleResize(() => cart.renderCartContents());
 // Get total to pay
 getTotalAmount(localStorage)
 // we getItem to show the number not the list of card and show on the bag
-document.querySelector('.icon-cart').innerHTML = localStorage.getItem('so-cart-quantity') || 0;
+//document.querySelector('.icon-cart').innerHTML = localStorage.getItem('so-cart-quantity') || 0;
