dd1049d8989435d1d407e02d71b901f53d65eca8
slnestun <riv21007@byui.edu>
2024-05-25 22:08:04 -0300

    try to solve netlify error

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -33,6 +33,6 @@ export default class ExternalServices {
       },
       body: JSON.stringify(payload),
     };
-    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+    return await fetch(baseURL + 'checkout/', options).then(res =>convertToJson(res));
   }
 }
